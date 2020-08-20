8a8958632ff64c7c13ae179067e79322554965aa
Andrew Morrison <andrewmorrison@newrelic.com>
2020-08-20 14:30:28 -0700

    Updates

--- precache-manifest.5d98a803b6603085ed6513e7ebb5a7a6.js
+++ precache-manifest.5d98a803b6603085ed6513e7ebb5a7a6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95fc587b6977ba55cd125f9f6beee42c",
+    "revision": "15ee3e919790cddcb84c9399117c2e9f",
     "url": "/traveling-freighter/index.html"
   },
   {
-    "revision": "25c8e10af908c3810458",
+    "revision": "8d39aff778737911d9b5",
     "url": "/traveling-freighter/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "cf91e8c6ed0334fba239",
-    "url": "/traveling-freighter/static/js/2.2c3dd1c9.chunk.js"
+    "revision": "d3b17891d6b91f38004a",
+    "url": "/traveling-freighter/static/js/2.866795cc.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/traveling-freighter/static/js/2.2c3dd1c9.chunk.js.LICENSE.txt"
+    "url": "/traveling-freighter/static/js/2.866795cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "25c8e10af908c3810458",
-    "url": "/traveling-freighter/static/js/main.32bb3f62.chunk.js"
+    "revision": "8d39aff778737911d9b5",
+    "url": "/traveling-freighter/static/js/main.dd0fe757.chunk.js"
   },
   {
     "revision": "931d1ba508b3fcbf1d7a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/traveling-freighter/precache-manifest.c0282b486be577eeab82ea194ecbff2c.js"
+  "/traveling-freighter/precache-manifest.5d98a803b6603085ed6513e7ebb5a7a6.js"
 );
 
 self.addEventListener('message', (event) => {
