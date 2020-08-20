a5e2a6e44a703741447ba5e3621e9de073d3ed9d
Andrew Morrison <andrewmorrison@newrelic.com>
2020-08-19 19:05:54 -0700

    Updates

---
+++ precache-manifest.f1df9c03bd74ba1d8df565b911b0884f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724d52f5c3c9851462f6723fd2a35669",
+    "url": "/traveling-freighter/index.html"
+  },
+  {
+    "revision": "855647dc4e29d0d8c48a",
+    "url": "/traveling-freighter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70c1482b965315e5cb91",
+    "url": "/traveling-freighter/static/js/2.536bee41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/traveling-freighter/static/js/2.536bee41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855647dc4e29d0d8c48a",
+    "url": "/traveling-freighter/static/js/main.04895f59.chunk.js"
+  },
+  {
+    "revision": "931d1ba508b3fcbf1d7a",
+    "url": "/traveling-freighter/static/js/runtime-main.8f004edb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/traveling-freighter/static/media/logo.5d5d9eef.svg"
+  }
+]);
